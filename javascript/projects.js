b8a49a741df2a8860df607a318125389d6e55f84
Oliver Catelan-Robinson <oscr1998@outlook.com>
2023-01-08 23:32:48 +0000

    project page

---
+++ javascript/projects.js
@@ -0,0 +1,18 @@
+// const gridItems = document.querySelectorAll('.project-item');
+
+// gridItems.forEach((item) => {
+//   item.addEventListener('mousemove', (event) => {
+//     const width = item.offsetWidth;
+//     const height = item.offsetHeight;
+//     const mouseX = event.pageX - item.offsetLeft - width / 2;
+//     const mouseY = event.pageY - item.offsetTop - height / 2;
+//     const depth = 25;
+    
+//     item.style.transform = `translateX(${-mouseX / depth}px) translateY(${-mouseY / depth}px)`;
+//   });
+  
+//   item.addEventListener('mouseleave', () => {
+//     item.style.transform = 'translate(0, 0)';
+//     item.style.transition = 'ease-in 0.2s'
+//   });
+// });
