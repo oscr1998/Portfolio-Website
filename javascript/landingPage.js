3768efb399044d495182221078139bc9175d8e9b
Oliver Catelan-Robinson <oscr1998@outlook.com>
2023-01-21 14:45:10 +0000

    additional animations

--- javascript/landingPage.js
+++ javascript/landingPage.js
@@ -1,3 +1,4 @@
+//! ########## Background mouse tracking ##########
 const left = document.getElementById("left-side");
 
 const handleMove = e => {
@@ -7,3 +8,39 @@ const handleMove = e => {
 document.onmousemove = e => handleMove(e);
 
 document.ontouchmove = e => handleMove(e.touches[0]);
+
+//! ########## Title Wave animation ##########
+
+var title = document.querySelector(".titleAnim");
+var letters = title.textContent.split("");
+title.textContent = "";
+for (var i = 0; i < letters.length; i++) {
+  var span = document.createElement("span");
+  span.textContent = letters[i];
+  span.style.animationDelay = (i / 10) + "s";
+  title.append(span);
+  if(i === 6){
+    var span = document.createElement("span");
+    span.textContent = ".";
+    span.style.visibility = "hidden"
+  title.append(span)
+  }
+}
+
+var title = document.querySelector(".titleAnim2");
+var letters = title.textContent.split("");
+title.textContent = "";
+for (var i = 0; i < letters.length; i++) {
+  var span = document.createElement("span");
+  span.textContent = letters[i];
+  span.style.animationDelay = (i / 10) + "s";
+  title.append(span);
+  if(i === 6){
+    var span = document.createElement("span");
+    span.textContent = ".";
+    span.style.visibility = "hidden"
+  title.append(span)
+  }
+}
+
+
